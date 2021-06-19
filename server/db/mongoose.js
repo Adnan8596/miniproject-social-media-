d309d7b806b71f45d39c719002eb22d927fd7a6f
naveen62 <coolnaveen62@gmail.com>
2021-06-20 01:00:39 +0530

    removed unnecessary code

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -8,24 +8,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/tasks-manager-api' ,{
     console.log(err)
 })
 
-
-// const me = new User({
-//     name:'    mikee   ',
-//     email:'mike@ds.inUUUUUU',
-//     password:'test passwor'
-// })
-
-// me.save().then((user) => {
-//     console.log(user)
-// }).catch((err) => {
-//     console.log(err)
-// })
-// const task = new Task({
-//     desc:'   Bring milk  '
-// })
-
-// task.save().then((data) => {
-//     console.log(data)
-// }).catch((err) => {
-//     console.log(err)
-// })
